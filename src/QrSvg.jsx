738da9708ae42ac4c91bceeee210ef4a5d488c93
Wojciech Maj <kontakt@wojtekmaj.pl>
2022-03-20 15:53:36 +0100

    Update QrSvg component

--- src/QrSvg.jsx
+++ src/QrSvg.jsx
@@ -1,50 +1,49 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 
-const QRCodeImpl = require('qr.js/lib/QRCode');
-const ErrorCorrectLevel = require('qr.js/lib/ErrorCorrectLevel');
+import QRCodeImpl from 'qr.js/lib/QRCode';
+import ErrorCorrectLevel from 'qr.js/lib/ErrorCorrectLevel';
 
 export default function QRCode({
-  value = '',
-  level = 'L',
-  bgColor = '#FFFFFF',
-  fgColor = '#000000',
+  bgColor = '#ffffff',
   cellClassPrefix = '',
+  fgColor = '#000000',
+  level = ErrorCorrectLevel.L,
+  value = '',
   ...otherProps
 } = {}) {
-  // adapted from https://github.com/zpao/qrcode.react/blob/master/src/index.js
-  const qrcode = new QRCodeImpl(-1, ErrorCorrectLevel[level]);
-  qrcode.addData(value);
-  qrcode.make();
+  const qrcode = useMemo(() => {
+    const qrcode = new QRCodeImpl(-1, ErrorCorrectLevel[level]);
+    qrcode.addData(value);
+    qrcode.make();
+    return qrcode;
+  }, [value, level]);
 
-  const cells = qrcode.modules;
+  const rows = qrcode.modules;
 
   const cellClassName = cellClassPrefix && `${cellClassPrefix}-cell`;
   const emptyCellClassName = cellClassPrefix && `${cellClassName} ${cellClassPrefix}-cell-empty`;
   const filledCellClassName = cellClassPrefix && `${cellClassName} ${cellClassPrefix}-cell-filled`;
 
-  let cellIndex = 0; // we use simple order as a key just to avoid the key warning here
+  let cellIndex = 0;
 
   return (
     <svg
       shapeRendering="crispEdges"
-      viewBox={[0, 0, cells.length, cells.length].join(' ')}
+      viewBox={[0, 0, rows.length, rows.length].join(' ')}
       {...otherProps}
     >
-      {cells.map((row, rowIndex) =>
+      {rows.map((row, rowIndex) =>
         row.map((cell, colIndex) => {
           const className = cell ? filledCellClassName : emptyCellClassName;
-          const classNameProp = className ? { className } : null;
+          const fill = cell ? fgColor : bgColor;
 
-          // Only use the fill if no cellClassPrefix was set. See https://github.com/no23reason/react-qr-svg/issues/136 for reasoning.
-          const fill = !classNameProp && (cell ? fgColor : bgColor);
-          const styleProp = fill ? { style: { fill } } : null;
           return (
             <rect
+              key={cellIndex++}
+              className={className}
+              fill={fill}
               height={1}
-              key={cellIndex++} // string was too slow here
-              {...styleProp}
-              {...classNameProp}
               width={1}
               x={colIndex}
               y={rowIndex}
