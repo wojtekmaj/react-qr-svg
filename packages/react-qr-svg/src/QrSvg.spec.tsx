bf3e0e532aa79c0b51b0f0f129fc8f07b00d0a3e
Wojciech Maj <kontakt@wojtekmaj.pl>
2025-10-30 10:54:04 +0100

    Migrate from RTL to vitest-browser-react

--- packages/react-qr-svg/src/QrSvg.spec.tsx
+++ packages/react-qr-svg/src/QrSvg.spec.tsx
@@ -1,11 +1,11 @@
 import { describe, expect, it } from 'vitest';
-import { render } from '@testing-library/react';
+import { render } from 'vitest-browser-react';
 
 import QrSvg from './QrSvg.js';
 
 describe('<QrSvg /> component', () => {
-  it('renders svg element properly', () => {
-    const { container } = render(<QrSvg value="Hello world" />);
+  it('renders svg element properly', async () => {
+    const { container } = await render(<QrSvg value="Hello world" />);
 
     const firstChild = container.children[0];
 
